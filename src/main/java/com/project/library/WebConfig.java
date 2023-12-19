09e3a3790d54e043833134eeb8964680ecefb293
d1lute <1875518693@qq.com>
2023-12-18 20:34:05 -0500

    final

--- src/main/java/com/project/library/WebConfig.java
+++ src/main/java/com/project/library/WebConfig.java
@@ -1,5 +1,7 @@
 package com.project.library;
 
+import java.io.File;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,10 +10,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    	String currentPath = new File(".").getAbsolutePath();
         registry.addResourceHandler("/images/**")
-                .addResourceLocations("classpath:/static/images/");
+                .addResourceLocations("file:/"+currentPath+"/src/main/resources/static/images/");
         registry.addResourceHandler("/epubs/**")
-        .addResourceLocations("classpath:/static/epubs/");
+        .addResourceLocations("file:/"+currentPath+"/src/main/resources/static/epubs/");
     }
 }
 
